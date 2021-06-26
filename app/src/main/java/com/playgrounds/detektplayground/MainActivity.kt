5e2ca2130d64146be958b97d56d36976cf5cf168
Shmuel Meymann <smeymann@lightricks.com>
2021-06-26 05:45:55 +0300

    Noise cleanup

--- app/src/main/java/com/playgrounds/detektplayground/MainActivity.kt
+++ app/src/main/java/com/playgrounds/detektplayground/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.playgrounds.detektplayground
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
